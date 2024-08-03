94c0bb9e2fa4babe6277f31dc101546f0ca975b3
Giovanni Cinel <giovanni.cinel@studenti.unipd.it>
2024-08-03 18:22:55 +0200

    CMake settings

---
+++ build/CMakeFiles/EasyParking.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EasyParking.dir/src/main.cpp.o"
+  "CMakeFiles/EasyParking.dir/src/main.cpp.o.d"
+  "EasyParking"
+  "EasyParking.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EasyParking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/EasyParking.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for EasyParking.
+# This may be replaced when dependencies are built.
