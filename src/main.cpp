6d0965bfde64c842cdccbfd309291aa6cc8f5173
GianNord <gianluca.nordio01@outlook.it>
2024-08-04 15:35:01 +0200

    Fixed CMakeLists to work with opencv and added test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <opencv2/highgui.hpp>
+#include <filesystem>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" <<std::filesystem::current_path()  << std::endl;
+    
+    cv::Mat img = cv::imread("../src/test.jpg");
+    cv::namedWindow("Test image");
+    cv::imshow("Test image", img);
+    cv::waitKey(0);
     return 0;
 }
