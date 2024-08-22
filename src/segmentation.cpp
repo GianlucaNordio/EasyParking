ef820b774f43ea953f129625d8f9f273091b9a45
GianNord <gianluca.nordio01@outlook.it>
2024-08-22 20:24:53 +0200

    testing with a threshold on the connected components (not bad)

--- src/segmentation.cpp
+++ src/segmentation.cpp
@@ -17,8 +17,28 @@ Segmentation::Segmentation(const std::vector<cv::Mat> &backgroundImages) {
 void Segmentation::segmentImage(const cv::Mat &image, cv::Mat &outputMask) {
     pBackSub -> apply(image, outputMask, BACKGROUND_NOT_UPDATED);
     //cv::blur(outputMask, outputMask, cv::Size(15, 15), cv::Point(-1, -1));
+    
     // Remove the shadow parts and the noise
-    //cv::threshold(outputMask, outputMask, 128, 255, cv::THRESH_BINARY);
+    // Tried using the noShade option in the constructor but still finds shades
+    cv::threshold(outputMask, outputMask, 128, 255, cv::THRESH_BINARY);
+
+    for(int k = 20; k < 5000; k+=500) {
+        // Keeping only connected that have a dimension bigger than 2000 pixels
+        cv::Mat stats, centroids, labelImage;
+        int numLabels = cv::connectedComponentsWithStats(outputMask, labelImage, stats, centroids, 8, CV_32S);
+        cv::Mat mask(labelImage.size(), CV_8UC1, cv::Scalar(0));
+        cv::Mat surfSup=stats.col(4) > k;
+        for (int i = 1; i < numLabels; i++) {
+            if (surfSup.at<uchar>(i, 0)) {
+                mask = mask | (labelImage==i);
+            }
+        }
+        cv::Mat r(outputMask.size(), CV_8UC1, cv::Scalar(0));
+        image.copyTo(r,mask);
+        imshow("Result", r);
+        cv::waitKey(500);
+        std::cout<<"Area " + k;
+    }
 }
 
 void Segmentation::segmentVectorImages(const std::vector<cv::Mat> &images, std::vector<cv::Mat> &outputMasks) {
