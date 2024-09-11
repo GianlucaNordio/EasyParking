b3cc3a72ee14af74b4207580a45564183caf2b1b
Giovanni Cinel <giovanni.cinel@studenti.unipd.it>
2024-09-11 16:25:34 +0200

    constructor

--- include/parkingSpot.hpp
+++ include/parkingSpot.hpp
@@ -10,6 +10,10 @@ class ParkingSpot {
         bool occupied;
         cv::RotatedRect rect;
 
+        ParkingSpot(int spotId, double spotConfidence, bool isOccupied, const cv::RotatedRect& spotRect)
+        : id(spotId), confidence(spotConfidence), occupied(isOccupied), rect(spotRect) {}
+        ParkingSpot() 
+        : id(0), confidence(0.0), occupied(false), rect(cv::RotatedRect()) {}
         void park();
         void leave();
         void displayStatus() const;
